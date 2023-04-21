97ca08a7eeabdd2e272d33bfc5f43710f36e1618
Jaden Sims <jadensims@u.boisestate.edu>
2023-04-20 19:06:03 -0600

    Null pointer on line 32 for all files.

--- TMSimulator.java
+++ TMSimulator.java
@@ -1,86 +1,108 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.*;
 
 public class TMSimulator {
-    private static int numStates; // Total number of states in the TM
-    private static int[][] transitionTable; // Transition function
-    private static int[] tape; // The tape
+    private int totalStates;
+    private int numSymbols;
+    private String inputString;
+    private int currState;
+    private LinkedList<Integer> tape;
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java TMSimulator <input_file>");
-            System.exit(1);
-        }
-
-        String inputFile = args[0];
+    public TMSimulator(String inputFilePath) {
         try {
-            readInput(inputFile);
-            simulate();
-            printTape();
+            BufferedReader br = new BufferedReader(new FileReader(inputFilePath));
+            totalStates = Integer.parseInt(br.readLine());
+            numSymbols = Integer.parseInt(br.readLine());
+            currState = 0;
+            tape = new LinkedList<>();
+            String transitionLine;
+            while ((transitionLine = br.readLine()) != null) {
+                String[] transition = transitionLine.split(",");
+                int nextState = Integer.parseInt(transition[0]);
+                int writeSymbol = Integer.parseInt(transition[1]);
+                char move = transition[2].charAt(0);
+                transitions.put(new Transition(currState, tape.isEmpty() ? 0 : tape.getLast(), nextState),
+                        new Operation(writeSymbol, move));
+                currState++;
+            }
+            inputString = br.readLine();
+            br.close();
+            for (char c : inputString.toCharArray()) {
+                tape.addLast(Character.getNumericValue(c));
+            }
+            tape.addFirst(0);
         } catch (IOException e) {
             e.printStackTrace();
-            System.exit(1);
         }
     }
 
-    private static void readInput(String inputFile) throws IOException {
-        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-
-        // Read the total number of states in the TM
-        numStates = Integer.parseInt(reader.readLine());
-
-        // Read the number of symbols in Σ
-        // Number of symbols in Σ
-        int numSymbols = Integer.parseInt(reader.readLine());
-
-        // Initialize the transition table
-        transitionTable = new int[numStates][numSymbols + 1];
-
-        // Read the transition function
-        for (int i = 0; i < numStates * (numSymbols + 1); i++) {
-            String[] tokens = reader.readLine().split(",");
-            int state = Integer.parseInt(tokens[0]);
-            int symbol = Integer.parseInt(tokens[1]);
-            int nextState = Integer.parseInt(tokens[2]);
-            int writeSymbol = Integer.parseInt(tokens[3]);
-            int move = tokens[4].equals("L") ? -1 : 1;
-            transitionTable[state][symbol] = nextState << 16 | writeSymbol << 8 | move;
+    public void run() {
+        Transition currTransition = new Transition(0, tape.isEmpty() ? 0 : tape.getLast(), 0);
+        while (transitions.containsKey(currTransition)) {
+            Operation operation = transitions.get(currTransition);
+            tape.removeLast();
+            tape.addLast(operation.writeSymbol);
+            if (operation.move == 'L') {
+                if (tape.getFirst() == 0) {
+                    tape.addFirst(0);
+                }
+                tape.removeFirst();
+            } else if (operation.move == 'R') {
+                if (tape.getLast() == 0) {
+                    tape.addLast(0);
+                }
+                tape.addLast(tape.removeFirst());
+            }
+            currState = currTransition.nextState;
+            currTransition = new Transition(currState, tape.getLast(), 0);
         }
-
-        // Read the input string
-        String inputString = reader.readLine();
-        tape = new int[inputString.length() + 2];
-        for (int i = 0; i < inputString.length(); i++) {
-            tape[i + 1] = inputString.charAt(i) - '0';
-        }
-
-        reader.close();
-    }
-
-    private static void simulate() {
-        // The position of the tape head
-        int headPosition = 1; // The tape head starts at position 1
-        // The current state of the TM
-        int currentState = 0; // The TM starts at state 0
-
-        while (currentState != numStates - 1) { // Stop when the TM reaches the halting state
-            int symbol = tape[headPosition];
-            int transition = transitionTable[currentState][symbol];
-            int nextState = transition >> 16;
-            int writeSymbol = (transition >> 8) & 0xff;
-            int move = transition & 0xff;
-
-            tape[headPosition] = writeSymbol;
-            headPosition += move;
-            currentState = nextState;
-        }
-    }
-
-    private static void printTape() {
-        for (int i = 1; i < tape.length - 1; i++) {
-            System.out.print(tape[i]);
+        for (int i : tape) {
+            System.out.print(i);
         }
         System.out.println();
     }
+
+    public static void main(String[] args) {
+        TMSimulator simulator = new TMSimulator(args[0]);
+        simulator.run();
+    }
+
+    private static class Transition {
+        private int currState;
+        private int inputSymbol;
+        private int nextState;
+
+        public Transition(int currState, int inputSymbol, int nextState) {
+            this.currState = currState;
+            this.inputSymbol = inputSymbol;
+            this.nextState = nextState;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Transition that = (Transition) o;
+            return currState == that.currState && inputSymbol == that.inputSymbol && nextState == that.nextState;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(currState, inputSymbol, nextState);
+        }
+    }
+
+    private static class Operation {
+        private int writeSymbol;
+        private char move;
+
+        public Operation(int writeSymbol, char move) {
+            this.writeSymbol = writeSymbol;
+            this.move = move;
+        }
+    }
+
+    private Map<Transition, Operation> transitions = new HashMap<>();
 }
